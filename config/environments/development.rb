343fb7495a63ee2d9ef251c4b1e4faa94cab7014
Vinicius Gabriel <vinihbieel49@gmail.com>
2023-01-06 09:06:36 -0300

    Change api response mime-type

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,6 +55,11 @@ Rails.application.configure do
 
   config.file_watcher = ActiveSupport::FileUpdateChecker
 
+  Rails.application.routes.default_url_options = {
+    host: 'localhost',
+    port: 3000
+  }
+
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
