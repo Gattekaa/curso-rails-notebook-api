30002bb0e00ae8735828e1c61ed024fd5282e7e0
Vinicius Gabriel <vinihbieel49@gmail.com>
2023-01-06 10:09:14 -0300

    create routes for has_many and has_one

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -2,9 +2,11 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
   belongs_to :kind do
-    link(:related) { kind_url(object.kind.id) }
+    link(:related) { contact_kind_url(object.id) }
   end
-  has_many :phones 
+  has_many :phones do
+    link(:related) { contact_kind_url(object.id) }
+  end
   has_one :address
 
 
