964ca451cd63e252ab5b329d0f6d661e32b0887d
Vinicius Gabriel <vinihbieel49@gmail.com>
2023-01-06 08:39:38 -0300

    Added json_api Adapter

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,3 +1,19 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
+
+  belongs_to :kind#, optional: true 
+  has_many :phones 
+  has_one :address
+
+  meta do
+    { author: "Teste" o}
+  end
+
+  def attributes(*args)
+    h = super(*args)
+    #h pt-BR [:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
+    h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
+    h
+  end
+
 end
